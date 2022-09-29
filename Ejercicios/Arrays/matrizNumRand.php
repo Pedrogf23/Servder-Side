ea1a258471b8b59d4ee0cbbd3dc73fda194f6846
Pedro <9827@cifpceuta.es>
2022-09-29 15:44:42 +0200

    29 09 22

--- Ejercicios/Arrays/matrizNumRand.php
+++ Ejercicios/Arrays/matrizNumRand.php
@@ -20,23 +20,20 @@
         }
     }
 
+    $mayor = $num[0];
+    $menor = $num[0];
     $k = 0;
     for($i = 0; $i < FILAS; $i++){
         for($j = 0; $j < COLUMNAS; $j++){
             $numBi[$i][$j] = $num[$k];
-            $k++;
-        }
-    }
-
-    $mayor = $numBi[0][0];
-    $menor = $numBi[0][0];
-    for($i = 0; $i < FILAS; $i++){
-        for($j = 0; $j < COLUMNAS; $j++){
             if($numBi[$i][$j] < $menor){
                 $menor = $numBi[$i][$j];
+                $filaMenor = $i;
             } else if ($numBi[$i][$j] > $mayor){
                 $mayor = $numBi[$i][$j];
+                $columnaMayor = $j;
             }
+            $k++;
         }
     }
 
@@ -44,9 +41,11 @@
     for($i = 0; $i < FILAS; $i++){
         echo "<tr>";
         for($j = 0; $j < COLUMNAS; $j++){
-            if($numBi[$i][$j] == $mayor){
+            if($i == $filaMenor && $j == $columnaMayor){
+                echo "<td style='background-color: aquamarine;'>". $numBi[$i][$j] ."</td>";
+            }else if($j == $columnaMayor){
                 echo "<td style='background-color: green;'>". $numBi[$i][$j] ."</td>";
-            } else if($numBi[$i][$j] == $menor){
+            } else if($i == $filaMenor){
                 echo "<td style='background-color: blue;'>". $numBi[$i][$j] ."</td>";
             } else {
                 echo "<td style='background-color: black; color: white;'>". $numBi[$i][$j] ."</td>";
