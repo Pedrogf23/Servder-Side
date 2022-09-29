3dfaf1cc67619e6b479baca620aa2f0f741da2d0
Pedro <9827@cifpceuta.es>
2022-09-29 15:37:55 +0200

    29 09 22

--- Ejercicios/Arrays/matrizNumRand.php
+++ Ejercicios/Arrays/matrizNumRand.php
@@ -10,9 +10,33 @@
     <?php
     const FILAS = 6;
     const COLUMNAS = 9;
+    for($i = 0; $i < FILAS*COLUMNAS; $i++){
+        $num[$i] = rand(100, 999);
+        for($j = 0; $j < $i; $j++){
+            while($num[$i] == $num[$j]) {
+                $num[$i] = rand(100, 999);
+                $j = 0;
+            }
+        }
+    }
+
+    $k = 0;
     for($i = 0; $i < FILAS; $i++){
         for($j = 0; $j < COLUMNAS; $j++){
-            $num[$i][$j] = rand(100, 999);
+            $numBi[$i][$j] = $num[$k];
+            $k++;
+        }
+    }
+
+    $mayor = $numBi[0][0];
+    $menor = $numBi[0][0];
+    for($i = 0; $i < FILAS; $i++){
+        for($j = 0; $j < COLUMNAS; $j++){
+            if($numBi[$i][$j] < $menor){
+                $menor = $numBi[$i][$j];
+            } else if ($numBi[$i][$j] > $mayor){
+                $mayor = $numBi[$i][$j];
+            }
         }
     }
 
@@ -20,7 +44,13 @@
     for($i = 0; $i < FILAS; $i++){
         echo "<tr>";
         for($j = 0; $j < COLUMNAS; $j++){
-            echo "<td>". $num[$i][$j] ."</td>";
+            if($numBi[$i][$j] == $mayor){
+                echo "<td style='background-color: green;'>". $numBi[$i][$j] ."</td>";
+            } else if($numBi[$i][$j] == $menor){
+                echo "<td style='background-color: blue;'>". $numBi[$i][$j] ."</td>";
+            } else {
+                echo "<td style='background-color: black; color: white;'>". $numBi[$i][$j] ."</td>";
+            }
         }
         echo "</tr>";
     }
