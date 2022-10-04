e6b77e12e9c5f243cca093b44552af8ffc123b99
Pedro <9827@cifpceuta.es>
2022-10-04 09:48:19 +0200

    04 10 22

---
+++ Ejercicios/Bucles/cantidadv3/cantidad.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cantidad</title>
+    <?php require_once('funciones.php'); ?>
+</head>
+<body>
+<?php
+if(isset($_POST['enviar1'])){
+    crearFormulario($_POST['cantidad']);
+} else if (isset($_POST['enviar2'])) {
+    sumar($_POST['cantidad']);
+} else {
+?>
+    <div style="width: 50%; margin: auto; margin-top: 6%;">
+        <form action="cantidad.php" method="post">
+            <table style="width: 50%; margin: auto; text-align: center;">
+                <tr>
+                    <th>Introduce una cantidad: </th>
+                </tr>
+                <tr>
+                    <td><input type="number" name="cantidad" placeholder="Cantidad..."></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" value="Enviar" name="enviar1"></td>
+                </tr>
+            </table>
+        </form>
+    </div>
+<?php
+}
+?>
+</body>
+</html>
---
+++ Ejercicios/Bucles/cantidadv3/funciones.php
@@ -0,0 +1,42 @@
+<?php
+
+function crearFormulario(int $cantidad){
+
+    echo '<div style="width: 50%; margin: auto; margin-top: 6%;">';
+        echo '<form action="cantidad.php" method="post">';
+            echo '<table style="width: 50%; margin: auto; text-align: center;">';
+                echo '<tr><th>Introduce <?php echo $cantidad; ?> números: </th></tr>';
+                for($i = 0; $i < $cantidad; $i++){
+                    echo "<tr><td><input type='number' name='n".$i."'></td></tr>";
+                }
+                echo '<tr><td><input type="submit" value="Sumar" name="enviar2"></td></tr>';
+            echo '</table>';
+            echo "<input type='hidden' name='cantidad' value='$cantidad'>";
+        echo '</form>';
+    echo '</div>';
+
+}
+
+function sumar(int $cantidad){
+
+    $cantidad = $_POST['cantidad'];
+    $suma = 0;
+    for($i = 0; $i < $cantidad; $i++){
+        $suma += $_POST['n'.$i];
+    }
+    ?>
+    <div style="width: 50%; margin: auto; margin-top: 6%;">
+        <table style="width: 50%; margin: auto; text-align: center;">
+            <tr>
+                <th>La suma es...</th>
+            </tr>
+            <tr>
+                <td><?php echo $suma; ?></td>
+            </tr>
+        </table>
+    </div>
+    <?php
+
+}
+
+?>
