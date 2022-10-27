bb14e278b8bf3b189efdda8be8b63837d522174f
Pedro <9827@cifpceuta.es>
2022-10-27 14:03:43 +0200

    27 10 22

---
+++ BBDD/conexion.php
@@ -0,0 +1,21 @@
+<?php
+// Abrir la conexión.
+function conectar(){
+    $host = "";
+    $user = "";
+    $pass = "";
+    $db_name = "";
+
+    $mysqli = new mysqli($host, $user, $pass, $db_name);
+    if($mysqli->connect_errno()){
+        echo 'Fallo al conectar a MySQL: ('. $mysqli->connnect_errno() .')'. $mysqli->connect_error();
+    }
+    return $mysqli;
+}
+
+// Cerrar la conexión.
+function cerrarConexion($connectionVar){
+    $connectionVar->close();
+}
+
+?>
